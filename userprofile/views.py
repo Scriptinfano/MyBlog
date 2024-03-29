985907df84fb1b05463e0c96863eda98353fef0e
scriptinfano <scripterMonster@protonmail.com>
2024-03-29 19:29:39 +0800

    改进用户在输入错误密码的情况下跳转到一个空白页面的情况，更改为在登录界面表单的上方直接提示用户输入密码错误

--- userprofile/views.py
+++ userprofile/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .forms import UserLoginForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -22,15 +23,20 @@ def user_login(request):
                 login(request, user)
                 return redirect("article:article_list")
             else:
-                return HttpResponse("账号或密码输入有误。请重新输入~")
+                # 用户名或密码错误时，返回登录页面，并在页面弹出对话框提示用户
+                messages.error(request, "账号或密码输入有误，请重新输入")
+                return redirect("userprofile:login")
         else:
-            return HttpResponse("账号或密码输入不合法")
+            # 用户名或密码错误时，返回登录页面，并在页面弹出对话框提示用户
+            messages.error(request, "表单填写有误，请重新输入")
+            return redirect("userprofile:login")
     elif request.method == 'GET':
         user_login_form = UserLoginForm()
         context = {'form': user_login_form}
         return render(request, 'userprofile/login.html', context)
     else:
         return HttpResponse("请使用GET或POST请求数据")
+
 
 # 用户退出
 def user_logout(request):
